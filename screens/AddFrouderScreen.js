f0c536dfcae8da22f7ae5e420ed038ac83666728
Andrey <mefston19@gmail.com>
2019-08-21 15:31:25 +0300

    I added two content with Screens.

--- screens/AddFrouderScreen.js
+++ screens/AddFrouderScreen.js
@@ -1,18 +1,130 @@
 import React,{Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View,StyleSheet, TextInput, Button, ScrollView} from 'react-native';
 
 
 class AddFrouderScreen extends Component {
     static navigationOptions = {
         title: 'Добавить мошенника',
     };
+    constructor(props) {
+        super(props);
+        this.state = {
+            froudName: '',
+            froudNameLast: '',
+            froudNumber: '',
+            froudCreditCard: '',
+            froudDescription: '',
+        }
+    };
+    hadlerChangeName = (val)=>{
+        this.setState({
+            froudName: val
+        });
+    };
+    hadlerChangeNameLast = (val)=>{
+        this.setState({
+            froudNameLast: val
+        });
+    };
+    hadlerChangeNumber = (val)=>{
+        this.setState({
+            froudNumber: val
+        });
+    };
+    hadlerCreditCardNumber = (val)=>{
+        this.setState({
+            froudCreditCard: val
+        });
+    };
+    hadlerDescription = (val)=>{
+        this.setState({
+            froudDescription: val
+        });
+    };
+    submitInformation = () =>{
+
+    };
     render(){
         return (
-            <View>
-                <Text> AddFrouderScreen</Text>
+            <View style={styles.container}>
+                <View style={styles.form_titles}>
+                    <Text style={styles.title}>Добавте подробную информацию о мошеннике.</Text>
+                </View>
+                <TextInput style={styles.input}
+                           onChangeText={this.hadlerChangeName}
+                           placeholder='Имя'
+                           value={this.state.frousadName}
+                />
+                <TextInput style={styles.input}
+                           onChangeText={this.hadlerChangeNameLast}
+                           placeholder='Фамилия'
+                           value={this.state.frousadNameLast}
+                />
+                <TextInput style={styles.input}
+                           onChangeText={this.hadlerChangeNumber}
+                           placeholder='Номер телефона'
+                           value={this.state.froudNumber}
+                />
+                <TextInput style={styles.input}
+                           onChangeText={this.hadlerCreditCardNumber}
+                           placeholder='Номер кредитной карточки: '
+                           value={this.state.froudCreditCard}
+                />
+                <TextInput style={styles.inputDescription}
+                           onChangeText={this.hadlerDescription}
+                           placeholder='Описание: '
+                           value={this.state.froudDescription}
+                           numberOfLines={4}
+                           multiline={true}
+                />
+                <View style={styles.button}>
+                    <Button
+                        onPress={this.submitInformation}
+                        title="Отправить информацию"
+                        color="#5499C7"
+                    />
+                </View>
             </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#e8ebf4',
+        paddingTop: 90,
+    },
+    title:{
+        textAlign: 'center',
+        color: '#554433',
+        paddingBottom: 0,
+    },
+    input:{
+        width: 220,
+        borderBottomColor: '#909497',
+        borderBottomWidth: 2,
+        color:  '#909497',
+        textAlign: 'left',
+    },
+    form_titles:{
+        paddingBottom: 30,
+    },
+    button:{
+        marginTop: 20,
+        marginBottom: 20,
+    },
+    inputDescription:{
+        width: 220,
+        height: 150,
+        borderBottomColor: '#909497',
+        borderBottomWidth: 2,
+        color:  '#909497',
+        textAlign: 'left',
+
+    }
+});
+
 
 export default AddFrouderScreen;
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,17 +1,60 @@
 import React,{Component} from 'react';
-import {Text, View, Button, Alert} from 'react-native';
+import {Text, View, Button, Alert, StyleSheet,TextInput} from 'react-native';
 
 class SearchScreen extends Component {
     static navigationOptions = {
         title: 'Поиск мошенника',
     };
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchInfo: ''
+        }
+    };
+
+    handlerInputSearch = (val)=>{
+        this.setState({
+            searchInfo: val
+        });
+    };
     render(){
         return (
-            <View>
-                <Text> SearchScreen </Text>
+            <View style={styles.body}>
+                <TextInput
+                onChangeText={()=>{this.handlerInputSearch}}
+                placeholder='Введите известную информацию о мошеннике'
+                style={styles.searchInput}
+                numberOfLines={20}
+                multiline={true}
+                />
+                <Button
+                    onPress={this.submitISearch}
+                    title="Отправить информацию"
+                    color="#5499C7"
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    body:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e8ebf4',
+    },
+    searchInput:{
+        width: '90%',
+        borderColor: '#909497',
+        borderWidth: 1,
+        color:  '#909497',
+        textAlign: 'left',
+        marginBottom: 20,
+    },
+    button:{
+        marginTop: 20,
+    },
+});
 
 export default SearchScreen
