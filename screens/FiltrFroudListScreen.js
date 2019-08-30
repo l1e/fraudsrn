83e7c0821943764526456eb7c33dc1968a2c0345
Andrey <mefston19@gmail.com>
2019-08-31 01:21:31 +0300

    I made filter for word.

--- screens/FiltrFroudListScreen.js
+++ screens/FiltrFroudListScreen.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native'
 
+import Itemfroud from '../component/Itemfroud';
 
 class FiltrFroudListScreen extends Component {
     static navigationOptions = {
         title: 'Найдено мошенников',
     };
     render(){
+        // const { navigation } = this.props;
+        // const filtreddata = navigation.getParam('filtreddata', 'Big data.');
+        // console.log('Helllo 1');
+        // console.log(filtreddata);
+        // console.log('Helllo 1');
+
         return(
             <View style={Styles.body}>
-                <Text> Извините, данный функионал появится в ближайшем будущем.</Text>
+                {/*{this.state.items.length > 0 ? (*/}
+                    {/*<Itemfroud items={filtreddata} />*/}
+                {/*) : (*/}
+                    {/*<Text>{this.state.itemsStatus}</Text>*/}
+                {/*)}*/}
             </View>
         )
     }
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
 import {Text, View, ScrollView, Button, StyleSheet,TextInput, Alert} from 'react-native';
+import {db} from '../firebase/config';
+let itemsRef = db.ref('/items');
 
 let placeHolderColor= '#909497';
 let inputText= '#25282b';
@@ -13,17 +15,58 @@ class SearchScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            searchInfo: ''
+            searchInfo: '',
+            items: {},
+            filtredData:[],
         }
     };
     handlerInputSearch = (val)=>{
         this.setState({
             searchInfo: val
         });
+        let dataa = [];
+        itemsRef.on('value', snapshot => {
+            let data = snapshot.val();
+            let items = Object.values(data);
+            this.setState({ items });
+            // let item = items[1];
+            // let line = item.card+' '+item.desc+' '+ item.firstname+' '+item.lastname+' '+ item.phone ;
+            // console.log(this.state.items);
+            // console.log(line);
+            // console.log('exsist: '+line.includes('0834323773'));
+            // console.log('Filstred list'+this.checkExistWord(items, '2222 2222 3333 4444'));
+            // data.push(this.checkExistWord(items, this.state.searchInfo));
+            dataa = this.checkExistWord(items, this.state.searchInfo);
+            this.setState({filtredData: dataa});
+            console.log('Start dataa A');
+            console.log(dataa);
+            console.log('End data');
+
+        });
+    };
+
+    checkExistWord =(item,filtredWord)=>{
+        let list = [];
+        let includeWord = 0 ;
+        let exsistWordsArray=[];
+        for (let i= 0 ; item.length > i  ;i++){
+            let line = item[i].card+' '+item[i].desc+' '+ item[i].firstname+' '+item[i].lastname+' '+ item[i].phone;
+            if (line.includes(filtredWord)){
+                exsistWordsArray.push(item[i]);
+            };
+            // list.push(line);
+        };
+        return exsistWordsArray;
+    };
+    componentDidMount() {
+
+
     };
     submitISearch= () =>{
         if (this.state.searchInfo != '') {
-            this.props.navigation.navigate('FiltrFroud');
+
+            this.props.navigation.navigate('FiltrFroud',{
+                filtreddata: this.state.filtredData});
         }else{
             Alert.alert("Ошибка","Введите информацию для поиска");
         }
