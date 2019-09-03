a60da4f9bd1400583c0813d3dabe5f3eca8a43af
L1e <dabog19@yahoo.com>
2019-09-03 11:11:30 +0300

    I aded localization in first page

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,7 +1,43 @@
 import React,{Component} from 'react';
-import {Text, View, StyleSheet, TouchableOpacity, SafeAreaView} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, SafeAreaView, NativeModules } from 'react-native';
+
 
 class WelcomeScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            lang: ' nothing '
+        }
+    }
+    componentDidMount (){
+        this.translateText();
+    }
+    translateText = ()=>{
+        const getCurrentLanguage = NativeModules.I18nManager.localeIdentifier;
+        const translation_en = require("../localization/en.json");
+        const translation_ru = require("../localization/ru.json");
+        let curentLanguage= null;
+        switch (getCurrentLanguage) {
+            case 'en_US':
+                console.log('My langiase is English');
+                curentLanguage =translation_en ;
+                break;
+            case 'ru_RU' :
+                console.log('Мой язык русский');
+                curentLanguage =translation_ru ;
+                break;
+            case 'uk_UA':
+                console.log('Моя пісня соловїна');
+                curentLanguage =translation_ru ;
+                break;
+            default:
+                curentLanguage =translation_en ;
+        }
+        console.log('My current language is : '+curentLanguage);
+        this.setState({lang: curentLanguage});
+        console.log(this.state.lang);
+    };
+
     static navigationOptions = {
         header: null,
     };
@@ -13,26 +49,26 @@ class WelcomeScreen extends Component {
                         onPress={() => this.props.navigation.navigate('AddFrouder')}
                         style={styles.button}
                     >
-                        <Text style={styles.textButton}>Добавить мошенника</Text>
+                        <Text style={styles.textButton}>{this.state.lang.main_addFrouder}</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
                         onPress={() => this.props.navigation.navigate('Search')}
                         style={styles.button}
                     >
-                        <Text  style={styles.textButton}>Поиск мошенника</Text>
+                        <Text  style={styles.textButton}>{this.state.lang.main_addFrouder}</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity
                         onPress={() =>this.props.navigation.navigate('ListFrouder')}
                         style={styles.button}
                     >
-                        <Text  style={styles.textButton}>Список мошенников</Text>
+                        <Text  style={styles.textButton}>{this.state.lang.main_listFrouder}</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
                         onPress={() =>this.props.navigation.navigate('AboutApp')}
                         style={styles.aboutus}
                     >
-                        <Text  style={styles.textButton}>О приложении</Text>
+                        <Text  style={styles.textButton}>{this.state.lang.main_aboutApp}</Text>
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
